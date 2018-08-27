c8b20073a3d3c43a68e46dcb4fc29bdc3ee87fa2
David Bourke <davidrbourke@gmail.com>
2018-08-27 14:18:30 +0100

    Spinning Words kata completed

---
+++ CSharpKatas/CSharpKatas/SpinningWords.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace CSharpKatas
+{
+    public class SpinningWords
+    {
+        public static string SpinWordsOverNLetters(string sentence, int n)
+        {
+            return string.Join(" ", sentence.Split(' ')
+                .Select(w => w.Length >= n ? string.Join("", w.Reverse()) : w));
+        }
+    }
+}
---
+++ CSharpKatas/CSharpKatasTests/SpinningWordsTests.cs
@@ -0,0 +1,23 @@
+﻿using CSharpKatas;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpKatasTests
+{
+    public class SpinningWordsTests
+    {
+        [Theory]
+        [InlineData("Hey fellow warriors", "Hey wollef sroirraw")]
+        public void SpinWordsOverNLetters_ValidSentance_WordsSpun(string sentance, string expectedSentance)
+        {
+            // Arrange
+            int n = 5;
+
+            // Act
+            var result = SpinningWords.SpinWordsOverNLetters(sentance, n);
+
+            // Assert
+            result.Should().Be(expectedSentance);
+        }
+    }
+}
