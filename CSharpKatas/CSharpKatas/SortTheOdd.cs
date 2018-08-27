1d4fa20c37f71b75ab13009b5ce76fd351cca340
David Bourke <davidrbourke@gmail.com>
2018-08-27 17:32:42 +0100

    Sort the odd kata completed

---
+++ CSharpKatas/CSharpKatas/SortTheOdd.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace CSharpKatas
+{
+    public class SortTheOdd
+    {
+        public static int[] Sort(int[] array)
+        {
+            var odds = array
+                .Where(inp => inp % 2 != 0)
+                .OrderBy(inp => inp)
+                .ToArray();
+
+            int posOfOdds = 0;
+            return array
+                .Select(
+                    a => a % 2 == 0 ? a : odds[posOfOdds++]
+                ).ToArray();
+        }
+    }
+}
---
+++ CSharpKatas/CSharpKatasTests/SortTheOddTests.cs
@@ -0,0 +1,23 @@
+﻿using CSharpKatas;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpKatasTests
+{
+    public class SortTheOddTests
+    {
+        [Fact]
+        public void Sort_ValidInput_SortedOddsReturned()
+        {
+            // Arrange
+            var input = new int[] { 5, 3, 2, 8, 1, 4 };
+            var output = new int[] { 1, 3, 2, 8, 5, 4 };
+
+            // Act
+            var result = SortTheOdd.Sort(input);
+
+            // Assert
+            result.Should().BeEquivalentTo(output);
+        }
+    }
+}
