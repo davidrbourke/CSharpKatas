48cb9bb3a8c77cfa4ef0a36574631fe709c622e1
David Bourke <davidrbourke@gmail.com>
2018-12-01 12:49:56 +0000

    Discover pin from keys kata added.

---
+++ CSharpKatas/CSharpKatas/DiscoverPIN.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CSharpKatas
+{
+    public class DiscoverPIN
+    {
+        /// <summary>
+        /// My solution
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        public string GetPIN(string[] keys)
+        {
+            var answer = new List<string>();
+
+            foreach (var keySet in keys)
+            {
+                for (int i = 0; i < keySet.Length; i++)
+                {
+                    if (answer.IndexOf(keySet[i].ToString()) == -1)
+                    {
+                        answer.Add(keySet[i].ToString());
+                    }
+                }
+
+                for (int j = 0; j < 3; j++)
+                {
+                    var iOfA = answer.IndexOf(keySet[0].ToString());
+                    var iOfB = answer.IndexOf(keySet[1].ToString());
+                    var iOfC = answer.IndexOf(keySet[2].ToString());
+
+                    if (iOfA > iOfB)
+                    {
+                        Insert(answer, iOfA, iOfB);
+                    }
+                    if (iOfB > iOfC)
+                    {
+                        Insert(answer, iOfB, iOfC);
+                    }
+                    if (iOfA > iOfC)
+                    {
+                        Insert(answer, iOfA, iOfC);
+                    }
+                }
+
+            }
+
+            return string.Join("", answer);
+        }
+
+        void Insert(IList<string> answer, int lower, int upper)
+        {
+            string val = answer[upper];
+            answer.RemoveAt(upper);
+            answer.Insert(lower, val);
+        }
+
+        /// <summary>
+        /// Gareth's brute force solution
+        /// </summary>
+        /// <param name="keyLog"></param>
+        /// <returns></returns>
+        public string BruteForce(string[] keyLog)
+        {
+            var found = false;
+            var index = 10000000; // I chose this as a starting point because I can see the count of unique numbers
+            var answer = "";
+            while (!found)
+            {
+                answer = (index++).ToString();
+
+                // The Linq All method takes advantage of short-circuit evaluation.
+                // As soon as any number is matched the while will progress
+                found = keyLog.All(x =>
+                answer.IndexOf(x[0]) != -1 &&
+                answer.IndexOf(x[1]) != -1 &&
+                answer.IndexOf(x[2]) != -1 &&
+
+                answer.IndexOf(x[0]) < answer.IndexOf(x[1]) &&
+                answer.IndexOf(x[1]) < answer.IndexOf(x[2])
+                );
+            }
+
+            return answer;
+        }
+    }
+}
+
+/*
+
+
+    
+ */
---
+++ CSharpKatas/CSharpKatasTests/DiscoverPINTests.cs
@@ -0,0 +1,40 @@
+﻿using CSharpKatas;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace CSharpKatasTests
+{
+    public class DiscoverPINTests
+    {
+        [Fact]
+        public void GetPIN_Simple()
+        {
+            // 3845
+            var discoverPIN = new DiscoverPIN();
+            var result = discoverPIN.GetPIN(new string[] { "352", "384", "845", "352" });
+
+            Assert.Equal("38452", result);
+        }
+
+        [Fact]
+        public void GetPin_File()
+        {
+            var file = File.ReadAllLines("keylogger.txt");
+            var result = new DiscoverPIN().GetPIN(file);
+
+            Assert.Equal("73162890", result);
+        }
+
+        [Fact]
+        public void GetPin_BruteForce()
+        {
+            var file = File.ReadAllLines("keylogger.txt");
+            var result = new DiscoverPIN().BruteForce(file);
+
+            Assert.Equal("73162890", result);
+        }
+    }
+}
