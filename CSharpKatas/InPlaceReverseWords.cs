890e625a01802883781b01187d1dc69f6b851b38
David Bourke <davidrbourke@gmail.com>
2018-12-09 18:55:40 +0000

    Restructure of solution and addition of inplace reverse words

---
+++ CSharpKatas/InPlaceReverseWords.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharpKatas
+{
+    public class InPlaceReverseWords
+    {
+        public char[] ReverseWords(char[] unordered)
+        {
+            var i = 0;
+            var j = unordered.Length - 1;
+            ReverseWord(unordered, i, j);
+
+            int prevK = 0;
+            for(var k = 0; k < unordered.Length; k++)
+            {
+                if (unordered[k] == ' ')
+                {
+                    ReverseWord(unordered, prevK, k-1);
+                    prevK = k+1;
+                }
+                else if (k == unordered.Length - 1)
+                {
+                    ReverseWord(unordered, prevK, k);
+                }
+            }
+
+            return unordered;
+        }
+
+        private void ReverseWord(char[] unordered, int i, int j)
+        {
+
+            while (i < j)
+            {
+                var iVal = unordered[i];
+                unordered[i] = unordered[j];
+                unordered[j] = iVal;
+                i++;
+                j--;
+            }
+        }
+    }
+}
---
+++ CSharpKatasTests/InPlaceReverseWordsTests.cs
@@ -0,0 +1,44 @@
+﻿using CSharpKatas;
+using Xunit;
+
+namespace CSharpKatasTests
+{
+    public class InPlaceReverseWordsTests
+    {
+        [Fact]
+        public void ReverseWords_TwoWordsSameLength_Reversed()
+        {
+            var inPlaceReverseWords = new InPlaceReverseWords();
+            var result = inPlaceReverseWords.ReverseWords("cake test".ToCharArray());
+
+            Assert.Equal("test cake".ToCharArray(), result);
+        }
+
+        [Fact]
+        public void ReverseWords_FourWordsSameLength_Reversed()
+        {
+            var inPlaceReverseWords = new InPlaceReverseWords();
+            var result = inPlaceReverseWords.ReverseWords("cake test some love".ToCharArray());
+
+            Assert.Equal("love some test cake".ToCharArray(), result);
+        }
+
+        [Fact]
+        public void ReverseWords_SingleWord_Reversed()
+        {
+            var inPlaceReverseWords = new InPlaceReverseWords();
+            var result = inPlaceReverseWords.ReverseWords("cake".ToCharArray());
+
+            Assert.Equal("cake".ToCharArray(), result);
+        }
+
+        [Fact]
+        public void ReverseWords_WordOfDifferentLength_Reversed()
+        {
+            var inPlaceReverseWords = new InPlaceReverseWords();
+            var result = inPlaceReverseWords.ReverseWords("landed has eagle the".ToCharArray());
+
+            Assert.Equal("the eagle has landed".ToCharArray(), result);
+        }
+    }
+}
